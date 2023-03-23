ff3f3036609001e5a6894d371030bf595103b1ef
Raphael Coelho <raphael_coelho@hotmail.com.br>
2023-03-22 23:40:26 -0300

    feature: add filters

--- src/modules/clinics/service.ts
+++ src/modules/clinics/service.ts
@@ -1,22 +1,35 @@
 import axios, { AxiosInstance } from "axios";
 import states from "utils/states";
 import { DentalClinic, ExternalClinic, VetClinic } from "./schema";
+import moment from 'moment'
 
 export class ClinicsService {
 
-  async list(filters?: ListFilters){
+  async list(filters: ListFilters){
     let clinics: ExternalClinic[] = []
 
-    if(filters?.type === 'vet'){
+    if(filters.type === 'vet'){
       const {data: vetClinics} = await this.client.get<VetClinic[]>('/vet-clinics.json')
-
       clinics = vetClinics.map(({clinicName, stateCode, opening}) => ({name: clinicName, state: states[stateCode], opening}))
     }
 
-    if(filters?.type === 'dental'){
+    if(filters.type === 'dental'){
       const {data: dentalClinics} = await this.client.get<DentalClinic[]>('/dental-clinics.json')
-
       clinics = dentalClinics.map(({name, stateName, availability}) => ({name, state: stateName, opening:availability}))
+    }
+
+    const {name, state, available_at} = filters
+    if(name){
+      clinics = clinics.filter(c => c.name.toLowerCase() === name.toLowerCase())
+    }
+    if(state){
+      clinics = clinics.filter(c => c.state === states[state])
+    }
+    if(available_at){
+      clinics = clinics.filter(c => {
+        return moment(available_at, "HH:mm")
+          .isBetween(moment(c.opening.from, "HH:mm"), moment(c.opening.to, "HH:mm"))
+      })
     }
 
     return clinics
@@ -33,4 +46,7 @@ export class ClinicsService {
 
 export type ListFilters = {
   type: 'vet' | 'dental'
+  name?: string,
+  state?: string
+  available_at?: string
 }
